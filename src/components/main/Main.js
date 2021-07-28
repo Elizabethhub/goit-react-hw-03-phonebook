c6781b003ff9e5d68d9946992532aa1d47766cf6
Elizabeth <bagiraliza21@gmail.com>
2021-07-28 22:18:13 +0300

    update

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -33,8 +33,9 @@ class Main extends Component {
   componentDidMount() {
     const contacts = localStorage.getItem("contacts");
     const parsedContacts = JSON.parse(contacts);
-
-    this.setState({ contacts: parsedContacts });
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
